984e4f0616eaaeddb1737a479288616907237371
Akihiko SHIRAI, Ph.D <shirai@mail.com>
2025-04-15 13:51:09 -0700

    rebuild

--- build.py
+++ build.py
@@ -2,12 +2,17 @@ import subprocess
 import shutil
 import os
 
-# Clean public/ directory if it exists
-#if os.path.exists("public"):
-#    shutil.rmtree("public")
+# Clean local public/ if exists
+if os.path.exists("public"):
+    shutil.rmtree("public")
 
-# Install Python dependencies
+# Build mkdocs into local public/
 subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
-
-# Build MkDocs site into public/
 subprocess.run(["mkdocs", "build", "-d", "public"], check=True)
+
+# Move output to /vercel/output/static
+output_path = "/vercel/output/static"
+if os.path.exists(output_path):
+    shutil.rmtree(output_path)
+
+shutil.copytree("public", output_path)
